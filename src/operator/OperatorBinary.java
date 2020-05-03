f6a576da36a877ea090abedd2db47bedbe64f0de
gustavyugan <gustav.yuga@gmail.com>
2020-05-03 20:40:34 +0700

    Operator Binary Aritmatika

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,20 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variabel
+
+    String nama = "Gustav Yuga Nugroho";
+    int jumlahSks = 24;
+    double indeksPresentasiKumulatif = 3.99;
+
+    public static void main(String[] args ){
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
+}
